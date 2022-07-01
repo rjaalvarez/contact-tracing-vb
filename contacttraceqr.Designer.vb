f112d5435ed4e4b2f1468b45e5b6d80aaa598700
iamrja <rj123alv@gmail.com>
2022-07-01 19:53:57 +0800

    added scan qr button, picture box for camera UI

---
+++ contacttraceqr.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ContactTracingFormQR
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.scanQRBtn = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'scanQRBtn
+        '
+        Me.scanQRBtn.Location = New System.Drawing.Point(151, 374)
+        Me.scanQRBtn.Name = "scanQRBtn"
+        Me.scanQRBtn.Size = New System.Drawing.Size(139, 61)
+        Me.scanQRBtn.TabIndex = 0
+        Me.scanQRBtn.Text = "Scan QR"
+        Me.scanQRBtn.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(426, 356)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'ContactTracingFormQR
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(450, 447)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.scanQRBtn)
+        Me.Name = "ContactTracingFormQR"
+        Me.Text = "Contact Tracing QR"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents scanQRBtn As Button
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
